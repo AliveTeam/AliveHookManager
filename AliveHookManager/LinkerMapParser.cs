0e5446be1d7b46330895c05f520dfb1012cde21f
Michael Grima <mlgthatsme@hotmail.com.au>
2018-06-23 12:51:05 +1000

    First code commit

---
+++ AliveHookManager/LinkerMapParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliveHookManager
+{
+    class LinkerMapParser
+    {
+        public LinkerMapParser()
+        {
+            Functions = new List<LinkerMapFunction>();
+        }
+
+        public class LinkerMapFunction
+        {
+            public string Name { get; set; }
+            public string Object { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name}:{Object}";
+            }
+        }
+
+        public List<LinkerMapFunction> Functions { get; private set; }
+
+        public void Parse(string linkMapText)
+        {
+            Functions.Clear();
+
+            string[] splitLines = linkMapText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var l in splitLines)
+            {
+                if (l.StartsWith(" 0001:"))
+                {
+                    string[] funcSplit = l.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (funcSplit.Last() != "CODE")
+                    {
+                        string funcName = funcSplit[1];
+                        funcName = funcName.Remove(0, 1);
+                        int firstAtIndex = funcName.IndexOf('@');
+                        funcName = funcName.Substring(0, (firstAtIndex != -1) ? firstAtIndex : funcName.Length);
+
+                        string[] nameSplit = funcName.Split(new char[] { '_' },  StringSplitOptions.RemoveEmptyEntries);
+                        int address = 0;
+                        if (int.TryParse(nameSplit.Last(), System.Globalization.NumberStyles.HexNumber, null, out address) && address > 0x400000)
+                        {
+                            string[] funcObjectSplit = funcSplit.Last().Split(':');
+                            string functionObject = funcObjectSplit.Last().Split('.').First();
+
+                            if (funcObjectSplit.First() == "AliveLib")
+                                Functions.Add(new LinkerMapFunction() { Name = funcName, Object = functionObject });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AliveHookManager/MainWindow.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AliveHookManager
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        LinkerMapParser mParser = new LinkerMapParser();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mParser.Parse(File.ReadAllText("AliveDll.map"));
+
+            foreach(var f in mParser.Functions)
+            {
+                listBoxFunctions.Items.Add(f);
+            }
+
+            foreach (var f in mParser.Functions)
+            {
+                if (!listBoxGroups.Items.Contains(f.Object))
+                    listBoxGroups.Items.Add(f.Object);
+            }
+
+            if (File.Exists("hook_map.txt"))
+            {
+                string[] existingFuncs = File.ReadAllLines("hook_map.txt");
+
+                for (int i = 0; i < listBoxFunctions.Items.Count; i++)
+                {
+                    if (existingFuncs.Contains(((LinkerMapParser.LinkerMapFunction)listBoxFunctions.Items[i]).Name))
+                        listBoxFunctions.SetSelected(i, true);
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxFunctions.SelectedItems.Clear();
+
+            List<LinkerMapParser.LinkerMapFunction> selectedFunctions = new List<LinkerMapParser.LinkerMapFunction>();
+
+            foreach(var o in listBoxFunctions.Items)
+            {
+                LinkerMapParser.LinkerMapFunction func = (LinkerMapParser.LinkerMapFunction)o;
+                var selectedObjects = new List<object>();
+                foreach (var l in listBoxGroups.SelectedItems)
+                    selectedObjects.Add(l);
+
+                foreach (var l in listBoxGroups.Items)
+                {
+                    if (selectedObjects.Contains(l) && func.Object == (string)l)
+                    {
+                        selectedFunctions.Add(func);
+                        break;
+                    }
+                }
+            }
+
+            foreach(var o in selectedFunctions)
+            {
+                listBoxFunctions.SelectedItems.Add(o);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBoxGroups.Items.Count; i++)
+                listBoxGroups.SetSelected(i, true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            foreach(var f in listBoxFunctions.SelectedItems)
+            {
+                LinkerMapParser.LinkerMapFunction func = (LinkerMapParser.LinkerMapFunction)f;
+                strBuilder.AppendLine(func.Name);
+            }
+
+            File.WriteAllText("hook_map.txt", strBuilder.ToString());
+        }
+    }
+}
